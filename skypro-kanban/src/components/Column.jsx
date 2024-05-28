e6f24556de0034bac272b154f5e6e028b3c8b4c2
Ga3im <akbutin.gazim.2000@gmail.com>
2024-05-29 01:26:25 +0500

    добавил папаку components

---
+++ skypro-kanban/src/components/Column.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function Column1(){
+   return(
+ <div class="main__column column">
+   <div class="column__title">
+       <p>Без статуса</p>
+   </div>
+   <Card/>
+</div>)
+}
+export default Column1;
---
+++ skypro-kanban/src/components/popups/popUp.jsx
@@ -0,0 +1,16 @@
+import popExit from "./popExit";
+import popNewCard from "../Card";
+import popBrowse from "./popBrowse";
+
+function popUp(){
+    <>
+    <popExit/>
+    <><popBrowse/></>
+    
+    <popNewCard/>
+    </>
+
+
+    
+}
+export default popUp
