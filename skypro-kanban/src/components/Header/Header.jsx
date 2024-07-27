ece4359cdac60ab680c04146fa3c3c32950ea96a
Ga3im <152912210+Ga3im@users.noreply.github.com>
2024-07-27 13:48:43 +0500

    Update Header.jsx

--- skypro-kanban/src/components/Header/Header.jsx
+++ skypro-kanban/src/components/Header/Header.jsx
@@ -1,47 +1,63 @@
-import * as S from "./Header.styled.js"
-import { Container } from "../../GlobalStyle.styled"
-import { Link } from "react-router-dom"
-import { routes } from "../../router/routes.js"
+import * as S from "./Header.styled.js";
+import { Container } from "../../GlobalStyle.styled";
+import { Link } from "react-router-dom";
+import { routes } from "../../router/routes.js";
+import { useUserContext } from "../../context/useUserContext.js";
+import { useState } from "react";
 
+export const Header = ({ isOpen, setIsOpen, changeTheme, setChangeTheme }) => {
+  const { user } = useUserContext();
+  const [checked, setChecked] = useState(false);
+  const toggleOpenUser = () => {
+    setIsOpen(!isOpen);
+  };
 
+  const onChangeTheme = (e) => {
+    e.preventDefault();
+    setChecked(!checked);
+    localStorage.setItem("theme", checked);
+    setChangeTheme(changeTheme === "light" ? "dark" : "light");
+  };
 
-export const Header = ({ isOpen, setIsOpen, addCard, changeTheme, setChangeTheme}) =>{
-	
-	let person = JSON.parse((localStorage.getItem('person')))
-	const toggleOpenUser = () =>{
-		setIsOpen(!isOpen)
-	  }
-
-	const onChangeTheme = ()=>{
-		setChangeTheme(changeTheme === 'light'? 'dark' : 'light')
-	}
-
-    return(<S.Header>
-			<Container>
-				<S.HeaderBlock>
-					<S.HeaderLogo>
-						<a href="" target="_self"><img src="/logo.png" alt="logo"/></a>
-					</S.HeaderLogo>
-					{/* <S.HeaderLogo >
-						<a href="" target="_self"><img src="/logo_dark.png" alt="logo"/></a>
-					</S.HeaderLogo> */}
-					<S.Nav>
-						<S.ButtonNewTask
-						onClick={addCard}
-						id="btnMainNew"><a>Создать новую задачу</a></S.ButtonNewTask>
-						<S.HeaderUserName $isOpen={isOpen} onClick={toggleOpenUser} >{person.name}</S.HeaderUserName>
-						{isOpen && 
-						<S.HeaderPopUser>
-							<S.UserName>{person.name}</S.UserName>
-							<S.UserMail>{person.login}</S.UserMail>
-							<S.UserTheme>
-								<p>Темная тема</p>
-								<input defaultChecked={changeTheme === "dark"} onClick={onChangeTheme} type="checkbox" className="checkbox" name="checkbox"/>
-							</S.UserTheme>
-							<Link to={routes.exit}><S.ButtonExit type="button">Выйти</S.ButtonExit></Link>
-						</S.HeaderPopUser>}			
-					</S.Nav>					
-				</S.HeaderBlock>
-			</Container>			
-		</S.Header>)
-}
+  return (
+    <S.Header>
+      <Container>
+        <S.HeaderBlock>
+          <S.HeaderLogo>
+            <a href="" target="_self">
+              <img src="/logo.png" alt="logo" />
+            </a>
+          </S.HeaderLogo>
+          <S.Nav>
+            <Link to={routes.addcard}>
+              <S.ButtonNewTask id="btnMainNew">
+                Создать новую задачу
+              </S.ButtonNewTask>
+            </Link>
+            <S.HeaderUserName $isOpen={isOpen} onClick={toggleOpenUser}>
+              {user.name}
+            </S.HeaderUserName>
+            {isOpen && (
+              <S.HeaderPopUser onClick={(e) => e.stopPropagation()}>
+                <S.UserName>{user.name}</S.UserName>
+                <S.UserMail>{user.login}</S.UserMail>
+                <S.UserTheme>
+                  <p>Темная тема</p>
+                  <input
+                    defaultChecked={checked}
+                    onClick={onChangeTheme}
+                    type="checkbox"
+                    name="checkbox"
+                  />
+                </S.UserTheme>
+                <Link to={routes.exit}>
+                  <S.ButtonExit type="button">Выйти</S.ButtonExit>
+                </Link>
+              </S.HeaderPopUser>
+            )}
+          </S.Nav>
+        </S.HeaderBlock>
+      </Container>
+    </S.Header>
+  );
+};
