5a4a5c10118eec1be808d1432a41bf529d542552
Ga3im <152912210+Ga3im@users.noreply.github.com>
2024-07-27 13:50:58 +0500

    Update AppRoutes.jsx

--- skypro-kanban/src/router/AppRoutes.jsx
+++ skypro-kanban/src/router/AppRoutes.jsx
@@ -1,32 +1,49 @@
-import {BrowserRouter, Routes, Route }from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NotFound } from "../page/NotFound/NotFound.jsx";
 import { MainPage } from "../page/MainPage/MainPage.jsx";
 import { routes } from "./routes.js";
 import { Login } from "../page/Login/Login.jsx";
 import { Register } from "../page/Register/Register.jsx";
-import { useState } from "react";
 import { ProtectedRoute } from "./ProtectedRoute.jsx";
 import { ExitPage } from "../page/ExitPage/ExitPage.jsx";
 import { CardPage } from "../page/CardPage/CardPage.jsx";
+import { UserProvider } from "../context/UserContext.jsx";
+import { AddCardPage } from "../page/AddCardPage/AddCardPage.jsx";
+import { DataCardProvider } from "../context/DataCardContext.jsx";
+import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { dark, light } from "../theme.js";
 
-
-export const AppRoutes = () =>{
-  const[user, setUser] = useState(null)
-    return(
-      <BrowserRouter>  
-        <Routes>
-          <Route element={<ProtectedRoute user={user}/>}>
-            <Route path={routes.main} element={<MainPage
-              user={user}
-              setUser={setUser}/>}>     
-            <Route path={routes.exit} element={<ExitPage setUser={setUser}/>}/>
-            <Route path={routes.card} element={<CardPage/>}/>
-            </Route>
-          </Route>
-            <Route path={routes.login} element={<Login setUser={setUser}/>}/>                 
-            <Route path={routes.notFound} element={<NotFound/>}/>     
-            <Route path={routes.register} element={<Register setUser={setUser}/>}/>     
-        </Routes>  
-      </BrowserRouter>   
-    )
-}
+export const AppRoutes = () => {
+  const [changeTheme, setChangeTheme] = useState("light");
+  return (
+    <ThemeProvider theme={changeTheme === "light" ? light : dark}>
+      <UserProvider>
+        <DataCardProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route element={<ProtectedRoute />}>
+                <Route
+                  path={routes.main}
+                  element={
+                    <MainPage
+                      changeTheme={changeTheme}
+                      setChangeTheme={setChangeTheme}
+                    />
+                  }
+                >
+                  <Route path={routes.addcard} element={<AddCardPage />} />
+                  <Route path={routes.exit} element={<ExitPage />} />
+                  <Route path={routes.card} element={<CardPage />} />
+                </Route>
+              </Route>
+              <Route path={routes.login} element={<Login />} />
+              <Route path={routes.notFound} element={<NotFound />} />
+              <Route path={routes.register} element={<Register />} />
+            </Routes>
+          </BrowserRouter>
+        </DataCardProvider>
+      </UserProvider>
+    </ThemeProvider>
+  );
+};
